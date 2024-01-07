4473b256bbacb75387d0b190a596240cbdb63576
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-07 11:05:08 +0700

    ✨ feat(TS): add `Function length` question

---
+++ coding/function-length/function-length.ts
@@ -0,0 +1,6 @@
+// Function length: Implement a function that returns the number of parameters expected by a function.
+// https://www.greatfrontend.com/questions/javascript/function-length
+
+export default function functionLength(fn: Function): number {
+  return fn.length;
+}
