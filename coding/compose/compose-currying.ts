ac9f0bf13e7637cbdecdc1613ae2c64bd9e774e1
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-02-03 15:37:48 +0700

    🔥 chore: remove unused code

--- coding/compose/compose-currying.ts
+++ coding/compose/compose-currying.ts
@@ -1,14 +1,6 @@
 // Compose: Implement a function that takes multiple functions as arguments and returns a new function that applies those functions in reverse
 // https://www.greatfrontend.com/questions/javascript/compose
 
-export function compose1(...fns: Array<Function>): Function {
-  return (input: any) => {
-    return fns.reduceRight((result, fn) => {
-      return fn(result);
-    }, input);
-  };
-}
-
 export default function compose(...fns: Array<Function>): Function {
   return function (x: any): any {
     function apply(fn: Function, ...rest: Array<any>): any {
