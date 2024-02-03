4e3015bb3c20efc072ed07808bedeb6aa2ca8c62
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-02-03 19:25:20 +0700

    🎨 style(TS): I prefer 2 iteration loops

--- coding/insertion-sort/insertion-sort.ts
+++ coding/insertion-sort/insertion-sort.ts
@@ -4,13 +4,11 @@
 export default function insertionSort(arr: Array<number>): Array<number> {
   const n = arr.length;
   for (let i = 1; i < n; i++) {
-    const current = arr[i];
-    let j = i - 1;
-    while (j >= 0 && current < arr[j]) {
-      arr[j + 1] = arr[j];
-      j--;
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
     }
-    arr[j + 1] = current;
   }
   return arr;
 }
